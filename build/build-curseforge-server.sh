ad1b1aebefdc11cfc70c9f5707c2dd06630f672e
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-07-10 00:26:01 +0800

    [Fix] Trying To fix build errors

--- build/build-curseforge-server.sh
+++ build/build-curseforge-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-./packwiz curseforge export --side server -o ".build/${PACK_NAME}-${VERSION}.zip"
+go run github.com/packwiz/packwiz@latest curseforge export --side server -o ".build/${PACK_NAME}-${VERSION}.zip"
--- build/build-modrinth.sh
+++ build/build-modrinth.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-./packwiz modrinth export -o ".build/${PACK_NAME}-${VERSION}.mrpack"
+go run github.com/packwiz/packwiz@latest modrinth export -o ".build/${PACK_NAME}-${VERSION}.mrpack"
