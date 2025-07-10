477dbf0481d1a283f2d2c70aa93d4dc99fcbba56
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-07-11 00:34:24 +0800

    [Feat] Migrating to pakku

--- build/build-curseforge-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/usr/local/go/bin
-PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
-VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-mkdir -p .build && go run github.com/packwiz/packwiz@latest curseforge export --side server -o ".build/${PACK_NAME}-${VERSION}-curseforge-server.zip"
