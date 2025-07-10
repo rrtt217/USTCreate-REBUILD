e24e6ee835682941f2350bf4c3249f645ab32ac8
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-07-10 13:06:43 +0800

    [Fix] The Second Trial

--- build/build-curseforge-server.sh
+++ build/build-curseforge-server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export PATH=$PATH:/usr/local/go/bin
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
+go run github.com/packwiz/packwiz@latest refresh
 go run github.com/packwiz/packwiz@latest curseforge export --side server -o ".build/${PACK_NAME}-${VERSION}.zip"
