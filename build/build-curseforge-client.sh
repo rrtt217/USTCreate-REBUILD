4b06feb2ccc773a37b5a721b6954c8f85832cf09
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-07-10 13:11:09 +0800

    [Fix] The third trial

--- build/build-curseforge-client.sh
+++ build/build-curseforge-client.sh
@@ -2,5 +2,4 @@
 export PATH=$PATH:/usr/local/go/bin
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-go run github.com/packwiz/packwiz@latest refresh
-go run github.com/packwiz/packwiz@latest curseforge export --side client -o ".build/${PACK_NAME}-${VERSION}.zip"
+mkdir -p .build && go run github.com/packwiz/packwiz@latest curseforge export --side client -o ".build/${PACK_NAME}-${VERSION}-curseforge-client.zip"
--- build/build-modrinth.sh
+++ build/build-modrinth.sh
@@ -2,5 +2,4 @@
 export PATH=$PATH:/usr/local/go/bin
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-go run github.com/packwiz/packwiz@latest refresh
-go run github.com/packwiz/packwiz@latest modrinth export -o ".build/${PACK_NAME}-${VERSION}.mrpack"
+mkdir -p .build && go run github.com/packwiz/packwiz@latest modrinth export -o ".build/${PACK_NAME}-${VERSION}-modrinth.mrpack"
