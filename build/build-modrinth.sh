7673a8a4f0309825872e2b7e3c6bc986c54fa5ff
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-07-11 23:16:27 +0800

    [Chore] Fix Hash and scripts

--- build/build-modrinth.sh
+++ build/build-modrinth.sh
@@ -2,4 +2,4 @@
 export PATH=$PATH:/usr/local/go/bin
 PACK_NAME=$(grep -m 1 'name' pack.toml | cut -d '"' -f2)
 VERSION=$(grep -m 1 'version' pack.toml | cut -d '"' -f2)
-mkdir -p .build && ./packwiz modrinth export  -o ".build/${PACK_NAME}-${VERSION}-curseforge-client.zip"
+mkdir -p .build && ./packwiz modrinth export  -o ".build/${PACK_NAME}-${VERSION}-modrinth.zip"
